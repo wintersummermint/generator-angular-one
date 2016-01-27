15b1636e6ae6ddad0f0fcedee57d5b6acc937ddd
Nixon Galario <cypher@redmorph.com>
2016-01-27 15:24:57 +0800

    fixed grunt build on assets dist

--- app/templates/skeleton/Gruntfile.js
+++ app/templates/skeleton/Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
     copy: {
       main: {
         files: [
-          {src: ['assets/**'], dest: 'dist/assets'},
+          {expand: true, src: ['assets/**'], dest: 'dist/'},
           {src: ['bower_components/font-awesome/fonts/**'], dest: 'dist/',filter:'isFile',expand:true},
           {src: ['bower_components/bootstrap/fonts/**'], dest: 'dist/',filter:'isFile',expand:true}
           //{src: ['bower_components/angular-ui-utils/ui-utils-ieshiv.min.js'], dest: 'dist/'},
